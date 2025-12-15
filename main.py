387c0e53adb068adbd758e3418c91c71c8d9017e
a.hernandez.gtz@gmail.com <a.hernandez.gtz@gmail.com>
2025-12-16 08:53:10 +1100

    Add sphere option to main.py

--- main.py
+++ main.py
@@ -2,13 +2,15 @@
 from geometry.cylinder import volume_cylinder
 from geometry.box import volume_box
 from geometry.cone import volume_cone
+from geometry.sphere import volume_sphere
 
 def menu():
     print("\n=== 3D Geometry Volume Calculator ===")
     print("1. Volume of a Cylinder")
     print("2. Volume of a Box")
     print("3. Volume of a Cone")
-    print("4. Quit")
+    print("4. Volume of a Sphere")
+    print("5. Quit")
 
 def run():
     while True:
@@ -35,6 +37,11 @@ def run():
             print(f"Volume of cone: {v:.3f} m³")
 
         elif choice == "4":
+            r = float(input("Enter radius (m): "))
+            v = volume_sphere(r)
+            print(f"Volume of sphere: {v:.3f} m³")
+
+        elif choice == "5":
             print("Catch you later, mate!\n\n")
             break
 
